4004679214c1e9d620f4b091d117597dec507944
mSounak <m.sounak2000@gmail.com>
2026-05-14 21:54:04 +0530

    chore: scaffold astro project

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://msounak.dev",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: { theme: "github-light", wrap: true },
+  },
+});
